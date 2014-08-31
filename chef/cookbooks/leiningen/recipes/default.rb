e15e52e6a9d3c355613f9d565b7b876a4c3be345
Toby Tripp <toby.tripp+github@gmail.com>
2014-08-31 13:01:39 -0500

    Install a recent version of leiningen.
    Instead of the old version provided by apt.

--- chef/cookbooks/leiningen/recipes/default.rb
+++ chef/cookbooks/leiningen/recipes/default.rb
@@ -6,6 +6,16 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package "leiningen" do
-  action :install
+remote_file "/usr/local/bin/lein" do
+  source "https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein"
+  mode 0755
+  owner "root"
+  group "root"
+end
+
+execute "install leiningen" do
+  command "lein version"
+  user  node[:leiningen][:user]
+  group node[:leiningen][:group]
+  environment "HOME" => node[:leiningen][:home]
 end
--- chef/roles/client.rb
+++ chef/roles/client.rb
@@ -5,3 +5,11 @@ run_list(
   "recipe[leiningen]",
   "recipe[ruby_bundler]"
 )
+
+default_attributes(
+  "leiningen" => {
+    "user"  => "vagrant",
+    "group" => "vagrant",
+    "home"  => "/home/vagrant"
+  }
+)
